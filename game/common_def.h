cc2617b78358bb07220f1ee8acc1bd92efe37c7e
pyke <pykelysia0206@163.com>
2025-04-22 18:33:18 +0800

    添加子弹

---
+++ game/common_def.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef std::string string;
+
+typedef int WIDTH_INT;
+typedef int HIGH_INT;
+
+typedef int _SIZE;
+typedef int SPEED;
+typedef int LIFE;
+typedef int ATTACK;
+
+typedef int DISTANCE;
+
+constexpr HIGH_INT HIGH = 720;
+constexpr WIDTH_INT WIDTH = 1280;
+constexpr double pi = 3.1415926;
+
+constexpr _SIZE PLAYERSIZE = 50;
--- game/main.cpp
+++ game/main.cpp
@@ -4,26 +4,24 @@
 #include<math.h>
 #include<vector>
 
-#define HIGH 720
-#define WIDTH 1280
-
-#define PLAYERSIZE 50
-
-using namespace std;
+#include "common_def.h"
 
 class Player {
 public:
-	Player(int t_x, int t_y, int t_life, int t_attack, string t_name) :x(t_x), y(t_y), life(t_life), attack(t_attack), name(t_name) {};
+	Player(POINT t_position, int t_life, int t_attack, string t_name) :
+		position(t_position),
+		life(t_life),
+		attack(t_attack),
+		name(t_name)
+		{};
 	virtual ~Player() {}
 
-	int Get_x() { return x; }
-	void Set_x(int t_x) { x = t_x; }
+	POINT GetPosition() const { return position; }
+	//
 
-	int Get_y() { return y; }
-	void Set_y(int t_y) { y = t_y; }
-
-	string Get_name() { return name; }
-	void Set_name(string t_name) { name = t_name; }
+	string GetName() const { return name; }
+	void SetName(string t_name) { name = t_name; }
+	//
 
 	void ProcessEvent(ExMessage msg) {
 
@@ -66,6 +64,7 @@ public:
 			}
 		}
 	}
+	//
 
 	void Move() {
 
@@ -75,31 +74,34 @@ public:
 		if (vec_dir != 0) {
 			double x_real = x_dir / vec_dir;
 			double y_real = y_dir / vec_dir;
-			x += x_real * speed;
-			y += y_real * speed;
+			position.x += x_real * speed;
+			position.y += y_real * speed;
 		}
+		//
 
-		if (x + 50 >= WIDTH) x = WIDTH - 51;
-		if (y + 50 >= HIGH) y = HIGH - 51;
-		if (x - 50 <= 0) x = 50;
-		if (y - 50 <= 0) y = 50;
+		if (position.x + 50 >= WIDTH) position.x = WIDTH - 51;
+		if (position.y + 50 >= HIGH) position.y = HIGH - 51;
+		if (position.x - 50 <= 0) position.x = 50;
+		if (position.y - 50 <= 0) position.y = 50;
+		//
 
 	}
 
-	void Drow() {
-		circle(x, y, PLAYERSIZE);
+	void Drow() const {
+		circle(position.x, position.y, PLAYERSIZE);
 	}
+	//
 
 private:
-	int x;
-	int y;
-	int speed = 5;
-	int life;
-	int attack;
+	POINT position = { 0, 0 };
+	SPEED speed = 5;
+	LIFE life;
+	ATTACK attack;
 	bool is_move_up = false,
 		is_move_left = false,
 		is_move_down = false,
 		is_move_right = false;
+	//
 	string name;
 };
 
@@ -108,30 +110,48 @@ public:
 	Bullet(){}
 	~Bullet(){}
 
-	void Draw(){}
+	_SIZE GetRedius(void) const { return redius; }
+	DISTANCE GetOrbitalRedius() const { return orbitalRedius; }
+	//
+
+	POINT GetPosition(void) const { return position; }
+	void SetPosition(POINT newPosition) { position = newPosition; }
+	//
+
+	void Draw() const {
+		circle(position.x, position.y, redius);
+	}
+	//
 
 private:
-
+	POINT position = { 0, 0 };
+	_SIZE redius = 40;
+	DISTANCE orbitalRedius = 100;
+	//
 };
 
 class Monster {
 public:
 	Monster(int t_life, int t_attack) : life(t_life), attack(t_attack) {}
-
-	bool CheckPlayerCollision(const Player& player){
-		return false;
-	}
-	
-	void Move(const Player& player){}
-
-	void Drow() {}
-
 	virtual ~Monster() {}
 
+	bool CheckPlayerCollision(){return false;}
+	//
+
+	bool CheckBulletCollision() {return false;}
+	//
+	
+	void Move(const Player& player){}
+	//
+
+	void Drow() {}
+	//
+
 protected:
-	int life;
-	int attack;
+	LIFE life;
+	ATTACK attack;
 	POINT position = { 0,0 };
+	//
 };
 
 class M_test :public Monster {
@@ -144,6 +164,7 @@ public:
 			Down,
 			Right
 		};
+		//
 
 		SpawnEdge edge = (SpawnEdge)(rand() % 4);
 
@@ -168,11 +189,15 @@ public:
 		default:
 			break;
 		}
+		//
 	}
+	//
+
 	void Move(const Player& player) {
 		Player t_player = player;
-		int x_dir = t_player.Get_x() - position.x;
-		int y_dir = t_player.Get_y() - position.y;
+		POINT playerPosition = t_player.GetPosition();
+		int x_dir = playerPosition.x - position.x;
+		int y_dir = playerPosition.y - position.y;
 		double vec_dir = sqrt(x_dir * x_dir + y_dir * y_dir);
 		if (vec_dir != 0) {
 			double x_real = x_dir / vec_dir;
@@ -180,29 +205,56 @@ public:
 			position.x += x_real * speed;
 			position.y += y_real * speed;
 		}
+		//s
 	}
+	//
+
 	bool CheckPlayerCollision(const Player& player) {
-		Player t_player = player;
-		POINT checkPosition = { position.x + M_Width / 2 - t_player.Get_x(),position.y + M_High / 2 - t_player.Get_y() };
+		POINT playerPosition = player.GetPosition();
+		POINT checkPosition = { position.x + M_Width / 2 - playerPosition.x, position.y + M_High / 2 - playerPosition.y };
 		int distance = sqrt(checkPosition.x * checkPosition.x + checkPosition.y * checkPosition.y);
 		if (distance <= PLAYERSIZE) {
 			return true;
 		}
 		return  false;
 	}
-	void Drow() {
+	//
+
+	bool CheckBulletCollision(const Bullet& bullet) {
+		POINT bulletPosition = bullet.GetPosition();
+		POINT checkPosition = { position.x + M_Width / 2 - bulletPosition.x, position.y + M_High / 2 - bulletPosition.y };
+		int distance = sqrt(checkPosition.x * checkPosition.x + checkPosition.y * checkPosition.y);
+		if (distance <= bullet.GetRedius()) {
+			return true;
+		}
+		return false;
+	}
+	//
+
+	void Hurt(void) {
+		life = 0;
+	}
+
+	bool CheckAlive(void) const {
+		return life > 0;
+	}
+
+	void Drow() const {
 		setfillcolor(RED);
 		solidrectangle(position.x, position.y, position.x + M_Width, position.y + M_High);
 	}
+	//
 
 private:
-	int M_Width = 40;
-	int M_High = 50;
-	int speed = 3;
+	_SIZE M_Width = 40;
+	_SIZE M_High = 50;
+	SPEED speed = 3;
+	LIFE life = 1;
 	POINT position = { 0,0 };
+	//
 };
 
-void TryGenerateM_test(std::vector<M_test*>& M_test_list) {
+static void TryGenerateM_test(std::vector<M_test*>& M_test_list) {
 	const int INTERVAL = 100;
 	static int counter = 0;
 	if ((++counter) % INTERVAL == 0) {
@@ -210,14 +262,47 @@ void TryGenerateM_test(std::vector<M_test*>& M_test_list) {
 		M_test_list.push_back(new M_test(1, 1));
 	}
 }
+/*
+* 判断并生成怪物
+*/
 
+static void UpdataBulletPosition(std::vector<Bullet*> bulletList, const Player& player) {
+	const int fullAngle = 360;
+	static int angle = 0;
+	int deltaAngle = 360 / (1.0 * bulletList.size());
+	POINT playerPosition = player.GetPosition();
+	//
+	angle = (angle + 2) % fullAngle;
+	int counter = 0;
+	for (auto bullet : bulletList) {
+		double positionAngle = (1.0 * angle + counter * deltaAngle) / fullAngle ;
+		//printf("%lf\n", positionAngle);
+		positionAngle = positionAngle * (2 * pi);
+		DISTANCE distance = bullet->GetOrbitalRedius();
+		POINT position = {
+			int(playerPosition.x + distance * cos(positionAngle)),
+			int(playerPosition.y + distance * sin(positionAngle))
+		};
+		bullet->SetPosition(position);
+		counter++;
+	}
+}
+/*
+* 更新子弹位置
+*/
 
 int main() {
 
+	//initgraph(WIDTH, HIGH, EX_SHOWCONSOLE);
 	initgraph(WIDTH, HIGH);
 
-	Player player = Player(640, 360, 100, 10, "tester");
+	Player player = Player({ 640, 360 }, 100, 10, "tester");
 	std::vector<M_test*> M_test_list;
+	std::vector<Bullet*> bulletList;
+
+	for (int i = 0; i < 3;i++) {
+		bulletList.push_back(new Bullet());
+	}
 
 	ExMessage msg;
 
@@ -232,26 +317,46 @@ int main() {
 		}
 
 		//数据处理
+		//Player处理
+		player.Move();
+		UpdataBulletPosition(bulletList, player);
+
 		//Monster处理
 		TryGenerateM_test(M_test_list);
 		for (auto monster : M_test_list) {
 			monster->Move(player);
 		}
 
-		//Player处理
-		player.Move();
-
 		//绘图
 		setbkcolor(BLACK);
 		cleardevice();
+		player.Drow();
+		int counter = 0;
+		for (auto bullet : bulletList) {
+			bullet->Draw();
+		}
 		for (auto monster : M_test_list) {
 			monster->Drow();
 		}
-		player.Drow();
 		for (auto monster : M_test_list) {
 			if (monster->CheckPlayerCollision(player)) {
 				Sleep(1000);
 				return 0;
+			}
+		}
+		for (auto monster : M_test_list) {
+			for (auto bullet : bulletList) {
+				if (monster->CheckBulletCollision(*bullet)) {
+					monster->Hurt();
+				}
+			}
+		}
+		for (int i = 0;i < M_test_list.size();i++) {
+			M_test* monster = M_test_list[i];
+			if (!monster->CheckAlive()) {
+				std::swap(M_test_list[i], M_test_list.back());
+				M_test_list.pop_back();
+				delete monster;
 			}
 		}
 
